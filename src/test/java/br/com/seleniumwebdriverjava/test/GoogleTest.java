a39851f6df8792146a912823b3fdbea63943845b
Vanderson1022 <vanderson1022@gmail.com>
2023-10-22 20:05:56 -0300

    Inserindo POO na automação

--- src/test/java/br/com/seleniumwebdriverjava/test/GoogleTest.java
+++ src/test/java/br/com/seleniumwebdriverjava/test/GoogleTest.java
@@ -4,27 +4,15 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class GoogleTest {
+public class GoogleTest extends BaseTest{
     
-    private WebDriver driver;
-    private final String URL_BASE = "https://www.google.com.br/";
-    private final String CAMINHO_DRIVER = "src/test/java/br/com/seleniumwebdriverjava/resource/chromedriver.exe";
-
-    public void iniciar(){
-        System.setProperty("webdriver.chrome.driver", CAMINHO_DRIVER);
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get(URL_BASE);
-    }
-
+    
+    
     @Test
     public void devePesquisarNoGoogle(){
-        iniciar();
 
         WebElement inputPesquisa = driver.findElement(By.name("q"));
         inputPesquisa.sendKeys("Batata frita" + Keys.ENTER);
